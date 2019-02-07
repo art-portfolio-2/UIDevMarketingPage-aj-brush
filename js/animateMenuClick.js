1475cfaf3e286b625877d158493f9d3b66f6cf7a
ajb85 <ajbrush85@gmail.com>
2019-02-06 21:51:25 -0500

    Animated nav bar on clicks.  Only works on mobile view

--- js/animateMenuClick.js
+++ js/animateMenuClick.js
@@ -1,11 +1,13 @@
 const menuBars = document.querySelector(".menuBars");
 
 menuBars.addEventListener("click", e => {
-  const dropdown = document.querySelector(".dropdown");
   const nav = document.querySelector("header nav");
-  const diamond = document.querySelector(".menuEnd");
-  diamond.classList.toggle("clicked");
-  dropdown.classList.toggle("clicked");
+  const ddContainer = document.querySelector(".ddContainer");
+  if (ddContainer.classList.length > 1) {
+    ddContainer.classList.toggle("unclicked");
+    nav.classList.toggle("unclicked");
+  }
+  ddContainer.classList.toggle("clicked");
   nav.classList.toggle("clicked");
   menuBars.classList.toggle("clicked");
 });
